3f490bd619acd4b75b0bd238961f3e357ac17464
Kellen Watt <kbw6d9@mst.edu>
2017-08-31 11:13:14 -0500

    Add to repository

---
+++ lib/.languages.rb
@@ -0,0 +1,71 @@
+# It is VERY possible that all of the methods in this file are either incomplete or
+# just entirely broken.
+#
+# Keep an eye out for broken compilation, and fix the issues if/when it becomes a problem
+
+$languages = {
+  "c++": /\.(c|h)(pp)?$/,
+  java: /\.java$/,
+  go: /\.go$/,
+  python: /\.py$/,
+  "C#": /\.cs$/,
+  assembly: /\.(s|S|asm)$/,
+  d: /\.d$/,
+  ruby: /\.rb$/,
+  lisp: /\.lisp$/,
+  python3: /\.py3$/,
+  crystal: /\.cr$/
+}
+
+$exclusions = [
+  /rails/
+]
+
+def get_dependencies(files) 
+  dependency_list = Hash.new {|hash,k| hash[k] = []}
+  files.each do |file|
+    dependency_list[file]
+    File.open(file) do |fin|
+      fin.readline
+      while (line = fin.readline) =~ /^#/
+        dependency_list[file] << line.split[1].chomp if line =~ /depends/
+        dependency_list[file] << "main" if line =~ /main/
+      end
+    end
+  end
+  dependency_list
+end
+
+# Possible bug when only one file is included that is main
+def dependency_sort(files, dependencies)
+  ordered_files = []
+  main = nil
+  dependencies.each do |file, depends|
+    catch :included do
+      ordered_files.each.with_index do |of, i|
+        if depends.include?("main")
+          main = file
+          throw :included
+        elsif depends.include?(of)
+          ordered_files.insert(i, file)
+          throw :included
+        end
+      end
+      ordered_files.push(file)
+    end
+  end
+  ordered_files.reverse!.push main if main
+  ordered_files
+end
+
+def combine(out, files)
+  dependencies = get_dependencies(files)
+  files = dependency_sort(files, dependencies)
+  files.each do |f|
+    File.open(f) do |fin|
+      while line = fin.gets do
+        out.puts line if line !~ /^#/
+      end
+    end
+  end
+end
